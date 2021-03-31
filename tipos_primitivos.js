611e08b5772bb19b9c702dbf6f903bf714207b89
Moacir David <65923695+moacirdavidag@users.noreply.github.com>
2021-03-31 13:36:05 -0300

    Script dos tipos primitivos em JS adicionado

---
+++ tipos_primitivos.js
@@ -0,0 +1,69 @@
+// number, string, boolean, undefined, null e NaN
+
+// Number
+
+/* let num = 4.5;
+console.log(typeof(num));
+
+let num2 = 10;
+console.log(num + num2);
+
+let soma = num + num2;
+
+console.log(typeof(soma)); */
+
+// String
+
+/* let nome = "Moacir David de Almeida Gonçalves";
+console.log(typeof(nome));
+
+let primeiraLetraAlfabeto = "A";
+
+console.log(typeof(primeiraLetraAlfabeto));
+
+let numero = "7";
+console.log(typeof(numero));
+
+console.log(nome + numero); */
+
+// Boolean
+
+// true or false
+
+/* let diaNublado = false;
+
+if(diaNublado == true){
+    console.log("O dia está nublado!");
+} else {
+    console.log("O dia não está nublado!");
+} */
+
+// undefined
+
+/* let numero;
+console.log(numero);
+
+numero = 4031;
+console.log(numero); */
+
+// Null
+
+/* let pessoa = null;
+
+console.log(typeof(pessoa));
+
+console.log(pessoa); */
+
+// Not a Number (NaN)
+
+let nota1 = "A";
+let nota2 = 8;
+
+let media = (nota1 + nota2)/2;
+
+console.log(media);
+
+console.log(80/"B");
+
+console.log("Isaías" - 40 - 31);
+
