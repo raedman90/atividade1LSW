8a7f04e53ad753f9c06cfabf1ee29d57049f7b7f
Israel Oliveira <35226183+raedman90@users.noreply.github.com>
2021-04-07 18:27:35 -0300

    Add files via upload

---
+++ exemplo.js
@@ -0,0 +1,34 @@
+/*Estrutura léxica, variáveis e tipos primitivos*/
+/*CASE SENSITIVE*/
+var lastname, lastNeme;
+lastNeme = "Israel";
+lastname = "Oliveira";
+
+console.log(lastNeme);
+console.log(lastname);
+/*PALAVRAS RESERVADAS*/
+/*let break;
+break = 1;
+console.log(break);/*
+/*NOMEAR VARIAVEIS*/
+var a, b , c;
+a = 5; 
+b = 6;
+c = a + b;
+console.log(a);
+console.log(b);
+console.log(c);
+/*TIPAGEM DINÂMICA*/
+/*ASPAS SIMPLES/DUPLA E TEMPLATE LITERALS*/
+const estoque = {
+    id : 1,
+    nome : 'Parafuso',
+    preco : 34.6
+}
+const descricao = `${estoque.nome} por apenas R$ ${estoque.preco}`
+/*NOT A NUMBER (NaN)*/
+
+/*NULL E UNDEFINED*/
+/*CONVERSÃO IMPLÍCITA E EXPLÍCITA*/
+
+/*CONVERSÃO IMPLÍCITA E EXPLÍCITA*/
